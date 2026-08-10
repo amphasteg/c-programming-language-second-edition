78f0a9ba40d37345f7ff94a958690d7f5469886e
Zach Nichols <github@zachdnichols.com>
2026-08-10 10:09:59 -0500

    4-2

--- chapter4/4-2.c
+++ chapter4/4-2.c
@@ -10,20 +10,27 @@
    by e or E end optionally signed exponent.
 */
 
-#include <stdio.h>
 #include <ctype.h>
+#include <stdio.h>
 
 double atof(char s[]);
 
-int main() {}
+//You can modify this array to change the number
+//calculated, or, modify it to allow for an input
+//string
+int main() { 
+  char s[] = {'1', '2', '5', '4', '5', 'e', '-', '6', '\0'};
+  
+  printf("%12.12f", atof(s));
+}
 
 double atof(char s[]) {
-  double val, power;
-  int i, sign;
+  double val, power, notation_sign;
+  int i, sign, notation_val;
 
   for (i = 0; isspace(s[i]); i++)
     ;
-  sign = (s[i] == '-') ? -1 : 1; 
+  sign = (s[i] == '-') ? -1 : 1;
   if (s[i] == '+' || s[i] == '-')
     i++;
   for (val = 0.0; isdigit(s[i]); i++)
@@ -34,6 +41,19 @@ double atof(char s[]) {
     val = 10.0 * val + (s[i] - '0');
     power *= 10;
   }
+  if (s[i] == 'e' || s[i] == 'E')
+    i++;
+  notation_sign = (s[i] == '-') ? .1 : 1;
+  if (s[i] == '+' || s[i] == '-')
+    i++;
+  for (notation_val = 0.0; isdigit(s[i]); i++)
+    notation_val = 10 * notation_val + (s[i] - '0');
 
-  return sign * val / power;
+  val = sign * val / power;
+
+  for (int j = 0; j < notation_val; j++) {
+    val *= notation_sign;
+  }
+
+  return val;
 }
