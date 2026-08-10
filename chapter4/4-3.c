8f51ada9290465b9780f06bf15a73b116c4f781e
Zach Nichols <github@zachdnichols.com>
2026-08-10 17:37:08 -0500

    4-3

--- chapter4/4-3.c
+++ chapter4/4-3.c
@@ -4,6 +4,7 @@
    Given the basic framework, it's straightforward
    to extend the calculator. Add the modulus (%)
    operator and provisions for negative numbers.
+   test
 */
 
 #include <ctype.h>
