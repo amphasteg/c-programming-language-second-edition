b39cecda3ce73a8e29446d5e895031c6be74a96b
Zach Nichols <github@zachdnichols.com>
2026-08-11 13:05:10 -0500

    Starting this one over since there are bugs

--- chapter4/4-3.c
+++ chapter4/4-3.c
@@ -19,8 +19,8 @@ double pop();
 int getch(void);
 void ungetch(int c);
 
-// Reverse polish calculator
-int main() {
+/* reverse Polish calculator */
+main() {
   int type;
   double op2;
   char s[MAXOP];
@@ -31,6 +31,9 @@ int main() {
       push(atof(s));
       break;
     case '+':
+      push(pop() + pop());
+      break;
+    case '*':
       push(pop() * pop());
       break;
     case '-':
@@ -42,102 +45,83 @@ int main() {
       if (op2 != 0.0)
         push(pop() / op2);
       else
-        printf("Error: Divide by Zero\n");
-      break;
-    case '%':
-      op2 = pop();
-      if (op2 != 0.0)
-        push((int)pop() % (int)op2);
-      else
-        printf("Error: Modulus by Zero");
+        printf("error: zero divisor\n");
       break;
     case '\n':
       printf("\t%.8g\n", pop());
       break;
     default:
-      printf("Unknown command: %c", type);
+      printf("error: unknown command %s\n", s);
       break;
     }
   }
   return 0;
 }
 
-#define MAXVAL 100
+#define MAXVAL 100 /* maximum depth of val stack */
 
-int sp = 0;
-double val[MAXVAL];
+int sp = 0;         /* next free stack position */
+double val[MAXVAL]; /* value stack */
 
+/* push: push f onto value stack */
 void push(double f) {
   if (sp < MAXVAL)
     val[sp++] = f;
   else
-    printf("Error: stack is full, cannot push %g\n", f);
+    printf("error: stack full, can't push %g\n", f);
 }
 
+/* pop: pop and return top value from stack */
 double pop(void) {
   if (sp > 0)
     return val[--sp];
   else {
-    printf("Error: stack is empty");
+    printf("error: stack empty\n");
     return 0.0;
   }
 }
 
+#include <ctype.h>
+
+int getch(void);
+void ungetch(int);
+
+/* getop: get next character or numeric operand */
 int getop(char s[]) {
-  int i = 0;
-  int c;
+  int i, c;
 
   while ((s[0] = c = getch()) == ' ' || c == '\t')
     ;
-
   s[1] = '\0';
-
-  if (!isdigit(c) && c != '.' && c != '-') {
-    return c;
-  }
-
-  if (c == '-') {
-    int next = getch();
-    if (next == '\n' || next == ' ' || next == '\t') {
-      ungetch(next);
-      return c; // return '-' as operator
-    } else if (!isdigit(next) && next != '.') {
-      return next; // not a number
-    } else         // number like "-5", "-.6" etc, next is digit or '.'
-    {
-      s[++i] = c = next;
-    }
-  } else {
-    c = getch();
-  }
-
-  if (isdigit(c)) {
+  if (!isdigit(c) && c != '.')
+    return c; /* not a number */
+  i = 0;
+  if (isdigit(c)) /* collect integer part */
     while (isdigit(s[++i] = c = getch()))
       ;
-  }
-
-  if (c == '.') {
+  if (c == '.') /* collect fraction part */
     while (isdigit(s[++i] = c = getch()))
       ;
-  }
-
-  if (c != EOF) {
+  s[i] = '\0';
+  if (c != EOF)
     ungetch(c);
-  }
-
   return NUMBER;
 }
 
 #define BUFSIZE 100
 
-char buf[BUFSIZE];
-int bufp = 0;
+char buf[BUFSIZE]; /* buffer for ungetch */
+int bufp = 0;      /* next free position in buf */
 
-int getch(void) { return (bufp > 0) ? buf[bufp--] : getchar(); }
+int getch(void) /* get a (possibly pushed-back) character */
+{
+  return (bufp > 0) ? buf[--bufp] : getchar();
+}
 
-void ungetch(int c) {
+void ungetch(int c) /* push character back on input */
+{
   if (bufp >= BUFSIZE)
-    printf("Ungetch: too many characters");
+    printf("ungetch: too many characters\n");
   else
     buf[bufp++] = c;
 }
