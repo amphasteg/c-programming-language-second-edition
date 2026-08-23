3b8ce68b4bff5d2531eb8200f753948f441ed7cf
Zach Nichols <github@zachdnichols.com>
2026-08-22 19:51:49 -0500

    Start 4-12

---
+++ chapter4/4-12.c
@@ -0,0 +1,18 @@
+/*
+Exercise 4-12
+
+Adapt the ideas of printd to write a recursive
+version of itoa; that is, convert an integer into
+a string by calling a recursive routine.
+*/
+
+#include <stdio.h>
+
+#define MAX_LEN 1000
+
+void itoa(int, char[]);
+
+int main(void) {
+  int num = 3258;
+  char s[MAX_LEN]; 
+}
