4984f19db941d782cd70d0ce9216a04f05907665
Zach Nichols <github@zachdnichols.com>
2026-08-30 18:48:40 -0500

    5-10

--- chapter5/5-10.c
+++ chapter5/5-10.c
@@ -8,13 +8,14 @@
  * evaluates 2 * (3+4).
  */
 
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 #define MAXOP 100
 #define NUMBER '0'
 
-int getop(char*);
+int getop(char *, char *);
 void push(double);
 double pop(void);
 
@@ -23,6 +24,100 @@ int main(int argc, char *argv[]) {
   double op2;
   char s[MAXOP];
 
-  for (int i = 1; i <= argc; ) {
+  while (--argc > 0) {
+    ++argv;
+    type = getop(s, *argv);
+      switch (type) {
+      case NUMBER:
+        push(atof(s));
+        break;
+      case '+':
+        push(pop() + pop());
+        break;
+      case '*':
+        push(pop() * pop());
+        break;
+      case '-':
+        op2 = pop();
+        push(pop() - op2);
+        break;
+      case '/':
+        op2 = pop();
+        if (op2 != 0.0)
+          push(pop() / op2);
+        else
+          printf("error: zero divisor\n");
+        break;
+      case '%':
+        op2 = pop();
+        if (op2 != 0.0)
+          push((int)pop() % (int)op2);
+        else
+          printf("Error: zero modulo");
+        break;
+      default:
+        printf("error: unknown command %s\n", s);
+        break;
+      argc--;
+    }
   }
+
+  printf("\t%.8g\n", pop());
+  return 0;
+}
+
+#define MAXVAL 100
+
+int sp = 0;
+double val[MAXVAL];
+
+void push(double f) {
+  if (sp < MAXVAL)
+    val[sp++] = f;
+  else
+    printf("error: stack full, cannot push %g\n", f);
+}
+
+double pop(void) {
+  if (sp > 0)
+    return val[--sp];
+  else {
+    printf("error: stack empty\n");
+    return 0.0;
+  }
+}
+
+#include <ctype.h>
+
+int getop(char *s, char* arg) {
+  int c;
+
+  while ((*s = c = *arg++) == ' ' || c == '\t')
+    ;
+
+  *(s + 1) = '\0';
+
+  if (!isdigit(c) && c != '.' && c != '-')
+    return c;
+
+  if (c == '-') {
+    int next = *arg++; 
+    if (next == ' ' || next == '\t' || next == '\n' || !next) {
+      return c;
+    } else if (!isdigit(next) && next != '.')
+      return next;
+    else
+      *(++s) = c = next;
+  }
+
+  if (isdigit(c))
+    while (isdigit(*(++s) = c = *arg++)) 
+      ;
+
+  if (c == '.')
+    while (isdigit(*(++s) = c = *arg))
+      ;
+
+  *s = '\0';
+  return NUMBER;
 }
