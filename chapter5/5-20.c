0bed00d7b91838a12a4b2f1797a9ae57e161ddd4
Zach Nichols <github@zachdnichols.com>
2026-09-08 08:59:42 -0500

    Started 5-20

--- chapter5/5-20.c
+++ chapter5/5-20.c
@@ -1,8 +1,9 @@
 /*
- * Exercise 5-19
+ * Exercise 5-20
  *
- * Modify undcl so that it does not add redundant
- * parenthesis to declarations.
+ * Expand dcl to handle declarations with function
+ * argument types, qualifiers like const, and so
+ * on.
  */
 
 #include <ctype.h>
@@ -13,6 +14,9 @@
 
 enum { NAME, PARENS, BRACKETS };
 
+void dcl(void);
+void dirdcl(void);
+
 int gettoken(void);
 int tokentype;
 char token[MAXTOKEN];
@@ -21,36 +25,48 @@ char datatype[MAXTOKEN];
 char out[1000];
 
 int main(void) {
-  int type;
-  char temp[MAXTOKEN];
-
   while (gettoken() != EOF) {
-    strcpy(out, token);
-    while ((type = gettoken()) != '\n')
-      if (type == PARENS) {
-        if (out[0] == '*') {
-          sprintf(temp, "(%s)", out);
-          strcpy(out, temp);
-        }
-
-        strcat(out, token);
-      } else if (type == BRACKETS) {
-        if (out[0] == '*') {
-          sprintf(temp, "(%s)", out);
-          strcpy(out, temp);
-        }
-        sprintf(temp, "[%s]", token);
-        strcat(out, temp);
-      } else if (type == '*') {
-        sprintf(temp, "*%s", out);
-        strcpy(out, temp);
-      } else if (type == NAME) {
-        sprintf(temp, "%s %s", token, out);
-        strcpy(out, temp);
-      } else
-        printf("Invalid input at %s\n", token);
-    printf("%s\n", out);
+    strcpy(datatype, token);
+    out[0] = '\0';
+    dcl();
+    if (tokentype != '\n')
+      printf("syntax error\n");
+    printf("%s: %s %s\n", name, out, datatype);
   }
+  return 0;
+}
+
+void dcl(void) {
+  int ns;
+
+  for (ns = 0; gettoken() == '*';) /* count *'s */
+    ns++;
+  dirdcl();
+  while (ns-- > 0)
+    strcat(out, " pointer to");
+}
+
+/* dirdcl: parse a direct declarator */
+void dirdcl(void) {
+  int type;
+
+  if (tokentype == '(') { /* ( dcl ) */
+    dcl();
+    if (tokentype != ')')
+      printf("error: missing )\n");
+  } else if (tokentype == NAME)
+    strcpy(name, token);
+  else
+    printf("error: expected name or (dcl)\n");
+  while ((type = gettoken()) == PARENS ||
+         type == BRACKETS)
+    if (type == PARENS)
+      strcat(out, " function returning");
+    else {
+      strcat(out, " array");
+      strcat(out, token);
+      strcat(out, " of");
+    }
 }
 
 int gettoken(void) {
