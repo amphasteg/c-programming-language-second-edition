cc001fffacdefd039407a957472f39ee01cff48b
Zach Nichols <github@zachdnichols.com>
2026-09-15 18:05:28 -0500

    Continue 6-2

--- chapter6/6-2.c
+++ chapter6/6-2.c
@@ -10,8 +10,10 @@
  * set from the command line.
  */
 
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define MAXWORD 100
 
@@ -22,6 +24,7 @@ struct word {
 };
 
 struct first_chars {
+  char *first_chars;
   struct word *root_word;
   struct first_chars *left;
   struct first_chars *right;
@@ -31,32 +34,35 @@ struct first_chars {
 // name, and if not, then it is not a var.
 // e.g. 'int x' is a var,
 // but func(int) is not.
-const char *suffix_types[] = {"int", "char",
-                              "float", "double"};
-// Types that can be used alongside a suffix type
-// or on their own.
-// e.g. long int x OR long x
-const char *dual_types[] = {"long", "unsigned",
-                            "signed"};
-// Types cannot be used on their own and most
-// always preceed a var type.
-// e.g. const int x
-const char *prefix_types[] = {"const", "static",
-                              "extern", "struct"};
+const char *types[] = {"int", "char", "float",
+                       "double"};
+
+// Certain, non-alphanumeric characters in C are
+// allowed to be in the names of variables
+const char allowed_chars[] = {'_', '-'};
 
 // Number of identical chars. Defaults to 6
-int n_chars = 6;
+unsigned int n_chars = 6;
 
-int check_args(int, char *[]);
+int check_args(int, char **);
 int getword(char *, int);
+int compare_type(char *, const char *[], int);
 
 int main(int argc, char *argv[]) {
   if (check_args(argc, argv) == -1)
     return -1;
+
+  char word[MAXWORD];
+
+  while (getword(word, MAXWORD) != EOF) {
+    if (compare_type(word, types, 4) == 0 &&
+        getword(word, MAXWORD) != EOF) {
+    }
+  }
 }
 
-// Verifies arguments are formatted correctly and
-// sets n if they are
+// Verifies arguments are formatted correctly
+// and sets n if they are
 int check_args(int argc, char *argv[]) {
   if (argc > 2) {
     printf("Too many arguments : %d. The only "
@@ -92,4 +98,99 @@ int check_args(int argc, char *argv[]) {
   return 0;
 }
 
+// This modified getword function will return
+// everything has spaces inbetween it; similar
+// to something like Vim
+int getword(char *word, int lim) {
+  int c, in_str_const, getch(void);
+  void ungetch(int);
+
+  char *w = word;
+
+  while (isspace(c = getch()))
+    ;
+
+  if (c != EOF)
+    *w++ = c;
+
+  for (; --lim > 0; w++)
+    if (!isalnum(*w = getch()) &&
+        strchr(allowed_chars, *w) == NULL) {
+      ungetch(*w);
+      break;
+    }
+
+  *w = '\0';
+  return word[0];
+}
+
+// Compares word to given list of types
+int compare_type(char *word, const char *types[],
+                 int type_size) {
+  for (int i = 0; i < type_size; i++)
+    if (strcmp(word, types[i]) == 0)
+      return 0;
+
+  return -1;
+}
+
+#define BUFSIZE 100
+
+char buf[BUFSIZE];
+int bufp = 0;
+
+int getch(void) {
+  return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c) {
+  if (bufp >= BUFSIZE)
+    printf("ungetch: too many characters\n");
+  else
+    buf[bufp++] = c;
+}
+
+struct first_chars *first_char_alloc(void);
+struct word *word_alloc(void);
+char *strndupl(char *, unsigned int);
+struct word add_word(struct word *p, char* full_word);
+
+struct first_chars add_new_word(struct first_chars *p,
+                            char *full_word) {
+  if (p == NULL) {
+    p = first_char_alloc();
+    p->first_chars = strndupl(full_word, n_chars);
+    p->root_word = NULL;
+    p->left = p->right = NULL;
+    *p->root_word = add_word(p->root_word, full_word);
+  }
+}
+
+struct word add_word(struct word *p, char* full_word) {
+  int cond;
+  if (p == NULL) {
+    p = word_alloc();
+    p->word = strndupl(full_word, strlen(full_word));
+    p->left = p->right = NULL;
+  }
+  else if ((cond = strcmp(full_word, p->word)) < 0)
+    add_word(p->left, full_word);
+  else
+   add_word(p->right, full_word);
+
+  return *p;
+}
+
+char *strndupl(char *s, unsigned int size) {
+  char *p = (char *)malloc(size + 1);
+
+  if (p != NULL)
+    strncpy(p, s, size + 1);
+
+  return p;
+}
+
+struct first_chars *first_char_alloc(void) {
+  return (struct first_chars *) malloc(sizeof(struct first_chars));
+}
 
